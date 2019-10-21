930cf21947e8bc0a59bf76413365af95d709b0b4
Toinv <toinv@egdgroup.com>
2019-10-21 13:42:46 +0700

    toinv add edit.php

---
+++ edit.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+<div class="container">
+    <div action="" >
+        <div class="form-group">
+            <label>First Name</label>
+            <input type="text" name="firstName" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>Last Name</label>
+            <input type="text" name="lastName" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input type="text" name="email" class="form-control">
+        </div>
+        <div class="form-foter">
+            <button type="submit" name="submit">Save</button>
+            <button type="reset" name="reset">Reset</button>
+        </div>
+    </form>
+</div>
