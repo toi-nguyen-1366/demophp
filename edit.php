9e61d3aedd6d32d30d9b87557624d803f7395f96
Toinv <toinv@egdgroup.com>
2019-10-21 14:01:44 +0700

    toinv123

--- edit.php
+++ edit.php
@@ -4,7 +4,15 @@ if(isset($_POST['submit'])){
     $firtName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $email = $_POST['email'];
-    echo $firtName;
+    if(empty($firtName)){
+        $msg = "firtsname is not empty!";
+    }
+    if(empty($lastName)){
+        $msg = "lastname is not empty!";
+    }
+    if(empty($email)){
+        $msg = "email is not empty!";
+    }
 }
 ?>
 <div class="container">
@@ -12,14 +20,17 @@ if(isset($_POST['submit'])){
         <div class="form-group">
             <label>First Name</label>
             <input type="text" name="firstName" class="form-control">
+            <div class="" style="color: crimson"> <?php echo empty($msg) ?? $msg ?></div>
         </div>
         <div class="form-group">
             <label>Last Name</label>
             <input type="text" name="lastName" class="form-control">
+            <div class="" style="color: crimson"> <?php echo empty($msg) ?? $msg ?></div>
         </div>
         <div class="form-group">
             <label>Email</label>
             <input type="text" name="email" class="form-control">
+            <div class="" style="color: crimson"> <?php echo empty($msg) ?? $msg ?></div>
         </div>
         <div class="form-foter">
             <button type="submit" name="submit">Save</button>
