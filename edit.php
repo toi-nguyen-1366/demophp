b4220e4ea215fcb79633dcc6ae948e3ae896e0ae
Toinv <toinv@egdgroup.com>
2019-10-21 14:08:38 +0700

    toinv edit edit.phpa

--- edit.php
+++ edit.php
@@ -5,13 +5,13 @@ if(isset($_POST['submit'])){
     $lastName = $_POST['lastName'];
     $email = $_POST['email'];
     if(empty($firtName)){
-        $msg = "firtsname is not empty!";
+        $msg1 = "firtsname is not empty!";
     }
     if(empty($lastName)){
-        $msg = "lastname is not empty!";
+        $msg2 = "lastname is not empty!";
     }
     if(empty($email)){
-        $msg = "email is not empty!";
+        $msg3 = "email is not empty!";
     }
 }
 ?>
@@ -20,17 +20,17 @@ if(isset($_POST['submit'])){
         <div class="form-group">
             <label>First Name</label>
             <input type="text" name="firstName" class="form-control">
-            <div class="" style="color: crimson"> <?php echo empty($msg) ?? $msg ?></div>
+            <div class="" style="color: crimson"><?php echo empty($msg1) ? $msg1 : '' ?></div>
         </div>
         <div class="form-group">
             <label>Last Name</label>
             <input type="text" name="lastName" class="form-control">
-            <div class="" style="color: crimson"> <?php echo empty($msg) ?? $msg ?></div>
+            <div class="" style="color: crimson"><?php echo empty($msg2) ? $msg2 : '' ?></div>
         </div>
         <div class="form-group">
             <label>Email</label>
             <input type="text" name="email" class="form-control">
-            <div class="" style="color: crimson"> <?php echo empty($msg) ?? $msg ?></div>
+            <div class="" style="color: crimson"> <?php echo empty($msg3) ?? $msg3 ?></div>
         </div>
         <div class="form-foter">
             <button type="submit" name="submit">Save</button>
